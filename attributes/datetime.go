71ad8be7b429630ede0cdf6da6f6ffd44dc0d201
Michael Boreham <bor3ham@gmail.com>
2017-06-25 16:15:14 +1000

    Add placeholder for datetime attr

---
+++ attributes/datetime.go
@@ -0,0 +1,19 @@
+package attributes
+
+// import (
+// 	"time"
+// )
+
+// type Datetime struct {
+// 	ColumnName string
+// }
+
+// func (d Datetime) GetColumnNames() []string {
+// 	return []string{d.ColumnName}
+// }
+// func (d Datetime) GetColumnVariables() []interface{} {
+// 	var destination *time.Time
+// 	return []interface{}{
+// 		&destination,
+// 	}
+// }
