d5fb45413f7b2689915b8e1b7470a55b2b5ba942
Michael Boreham <bor3ham@gmail.com>
2017-06-27 16:53:53 +1000

    Add general instance pointer

--- instances/instance.go
+++ instances/instance.go
@@ -7,3 +7,21 @@ type Instance interface {
 	SetValues(values []interface{})
 	GetValues() []interface{}
 }
+
+type InstancePointer struct {
+	ID   *string `json:"id"`
+	Type string  `json:"type"`
+}
+
+func (ip *InstancePointer) GetID() string {
+	if ip.ID == nil {
+		return ""
+	}
+	return *ip.ID
+}
+func (ip *InstancePointer) SetID(id string) {
+	ip.ID = &id
+}
+func (ip *InstancePointer) GetType() string {
+	return ip.Type
+}
