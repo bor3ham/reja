48e082e3a2646f0a2636633fddfc24ff4ec7c810
Michael Boreham <bor3ham@gmail.com>
2017-06-30 13:01:04 +1000

    Remove temp function

--- relationships/relationship.go
+++ relationships/relationship.go
@@ -14,33 +14,6 @@ type PointerSet struct {
 	Data []instances.InstancePointer `json:"data"`
 }
 
-// temporary function to flatten list as part of refactor
-func flattenMaps(relationMap map[string]map[string][]string) map[string][]string {
-	flatMap := map[string][]string{}
-	for _, relations := range relationMap {
-		for modelType, ids := range relations {
-			_, exists := flatMap[modelType]
-			if !exists {
-				flatMap[modelType] = []string{}
-			}
-			flatMap[modelType] = append(flatMap[modelType], ids...)
-		}
-	}
-	// unique the list of ids
-	distinctMap := map[string][]string{}
-	for model, ids := range flatMap {
-		distinctMap[model] = []string{}
-		distincts := map[string]bool{}
-		for _, id := range ids {
-			distincts[id] = true
-		}
-		for id, _ := range distincts {
-			distinctMap[model] = append(distinctMap[model], id)
-		}
-	}
-	return distinctMap
-}
-
 func AssertPointerSet(val interface{}) PointerSet {
 	pageVal, ok := val.(PointerSet)
 	if !ok {
