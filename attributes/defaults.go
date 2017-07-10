92c960ea17b5064b45a25a48bf94380b4f61597a
Michael Boreham <bor3ham@gmail.com>
2017-07-10 15:28:34 +1000

    Improve attribute default helpers

--- attributes/defaults.go
+++ attributes/defaults.go
@@ -4,10 +4,20 @@ import (
 // "github.com/bor3ham/reja/schema"
 )
 
-func DefaultBlankText(instance interface{}) TextValue {
-	blank := ""
-	return TextValue{
-		Provided: true,
-		Value:    &blank,
+func DefaultText(textValue string) (func(interface{}) TextValue) {
+	return func(instance interface{}) TextValue {
+		return TextValue{
+			Provided: true,
+			Value: &textValue,
+		}
+	}
+}
+
+func DefaultBool(boolValue bool) (func(interface{}) BoolValue) {
+	return func(instance interface{}) BoolValue {
+		return BoolValue{
+			Provided: true,
+			Value: &boolValue,
+		}
 	}
 }
